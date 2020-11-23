2c09c616dd580d2c9aa8acdbf5ee24b84b4739c7
Fahd <fahd.boudali@nordnet.com>
2020-11-23 13:54:20 +0100

    init

---
+++ src/main/java/com/fboudali/training/springbootlombokmapstruct/SpringBootLombokMapstructApplication.java
@@ -0,0 +1,13 @@
+package com.fboudali.training.springbootlombokmapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLombokMapstructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLombokMapstructApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fboudali/training/springbootlombokmapstruct/SpringBootLombokMapstructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fboudali.training.springbootlombokmapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLombokMapstructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
